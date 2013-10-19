aae729a93292beca15f169d02548f34254070792
Tomasz Durka <tomasz@durka.pl>
2013-10-19 05:50:11 +0200

    Add list command to list all repo issues, --mine flag added

---
+++ lib/githubissues/command/list.rb
@@ -0,0 +1,25 @@
+module GithubIssues
+  class Command::List < Command
+
+    option "--mine", :flag, "show only mine issues"
+
+    def execute
+      github_repo = get_github_repo
+      issues_client = Github::Issues.new
+      request = {:user => github_repo[:user], :repo => github_repo[:name]}
+      request.store(:assignee, @username) if mine?
+      issues = issues_client.list request
+
+      issues.each do |issue|
+        if not issue.assignee.nil? and issue.assignee.login == @username
+          print on_yellow ' '
+        else
+          print ' '
+        end
+        print bold ' ' + ('#' + issue.number.to_s).rjust(6)
+        print ' ' + issue.title
+        puts
+      end
+    end
+  end
+end
--- lib/githubissues/command_manager.rb
+++ lib/githubissues/command_manager.rb
@@ -1,9 +1,10 @@
 module GithubIssues
   class Command_manager < Clamp::Command
 
+    subcommand "list", "Lists issues", Command::List
+    subcommand "checkout", "Checkouts specifc issue", Command::Checkout
+    subcommand "show", "Show current issue details", Command::Show
     subcommand "browse", "Navigate to issue HTML url", Command::Browse
     subcommand "open", "Open new issue", Command::Open
-    subcommand "show", "Show current issue details", Command::Show
-    subcommand "checkout", "Checkouts specifc issue", Command::Checkout
   end
 end
