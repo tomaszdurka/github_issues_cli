f714d9da89a20539105b358483ee8e9f009f4632
Tomasz Durka <tomasz@durka.pl>
2013-10-19 05:09:33 +0200

    Make sure upstream is fetched before checkout, make issues_client inline

--- lib/githubissues/command/open.rb
+++ lib/githubissues/command/open.rb
@@ -5,11 +5,12 @@ module GithubIssues
 
     def execute
       github_repo = get_github_repo
-      issues_client = Github::Issues.new :user => github_repo[:user], :repo => github_repo[:name]
-      issue = issues_client.create :title => summary
+      issues_client = Github::Issues.new.create :user => github_repo[:user], :repo => github_repo[:name], :title => summary
       issue_number = issue[:number].to_s
 
+      get_github_repo
       git_repo = get_git_repo
+      git_repo.remote('upstream').fetch
       git_repo.lib.checkout 'upstream/master', :new_branch => 'issue-' + issue_number
       print on_green ' '
       print ' Checked out '
