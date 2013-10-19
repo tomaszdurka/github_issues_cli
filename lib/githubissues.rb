c33a4f68ac04c50effbcf2224cc4a64bbe3645fd
Tomasz Durka <tomasz@durka.pl>
2013-10-19 05:08:46 +0200

    Add missing io/console for authenticate

--- lib/githubissues.rb
+++ lib/githubissues.rb
@@ -3,10 +3,10 @@ module GithubIssues
   require 'git'
   require 'term/ansicolor'
   require 'github_api'
+  require 'io/console'
   require 'githubissues/command'
   require 'githubissues/command/browse'
   require 'githubissues/command/show'
   require 'githubissues/command/open'
-  require 'githubissues/command/checkout'
   require 'githubissues/command_manager'
 end
