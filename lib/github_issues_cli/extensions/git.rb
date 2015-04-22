7b1266b66858a9264a4817b9e7ba0cf027343532
tomaszdurka <tomasz@durka.pl>
2015-04-22 03:19:51 +0200

    Debug info

--- lib/github_issues_cli/extensions/git.rb
+++ lib/github_issues_cli/extensions/git.rb
@@ -1,6 +1,7 @@
 module Git
   class Lib
     def command_proxy(cmd, opts = [], chdir = true, redirect = '', &block)
+      puts cmd + opts.join(' ')
       command(cmd, opts, chdir, redirect, &block)
     end
   end
