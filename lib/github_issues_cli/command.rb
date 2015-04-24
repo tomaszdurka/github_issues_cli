e1233148dc31b75247d8839410ce3d708c180283
tomaszdurka <tomasz@durka.pl>
2015-04-24 02:31:22 +0200

    Bring back run wrapper

--- lib/github_issues_cli/command.rb
+++ lib/github_issues_cli/command.rb
@@ -109,5 +109,20 @@ module GithubIssuesCli
       source_remote.fetch
       source_remote.name + '/' + branch_name
     end
+
+    def run(arguments)
+      begin
+        super
+      rescue Exception => e
+        print on_red ' '
+        print bold ' Error: '
+        if e.message.empty?
+          puts 'Unknown error/Interrupt'
+        else
+          puts e.message
+        end
+        exit 1
+      end
+    end
   end
 end
